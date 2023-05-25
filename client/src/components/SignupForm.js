431801199e0aa8b0ef837e8717e0603dff7a9c49
Onelle11 <ofullente@gmail.com>
2023-05-25 00:48:03 -0400

    updated all my files

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
+/* import { createUser } from '../utils/API'; */
+import { useMutation } from "@apollo/react-hooks";
+import { ADD_USER } from "../utils/mutations";
+
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -11,6 +14,16 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  const [addUser, { error }] = useMutation(ADD_USER);
+
+  useEffect(() => {
+    if (error) {
+      setShowAlert(true);
+    } else {
+      setShowAlert(false);
+    }
+  }, [error]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -27,7 +40,7 @@ const SignupForm = () => {
       event.stopPropagation();
     }
 
-    try {
+    /* try {
       const response = await createUser(userFormData);
 
       if (!response.ok) {
@@ -40,6 +53,16 @@ const SignupForm = () => {
     } catch (err) {
       console.error(err);
       setShowAlert(true);
+    } */
+
+    try {
+      const { data } = await addUser({
+        variables: { ...userFormData },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
     }
 
     setUserFormData({
@@ -58,7 +81,7 @@ const SignupForm = () => {
           Something went wrong with your signup!
         </Alert>
 
-        <Form.Group className='mb-3'>
+        <Form.Group /* className='mb-3' */>
           <Form.Label htmlFor='username'>Username</Form.Label>
           <Form.Control
             type='text'
@@ -71,7 +94,7 @@ const SignupForm = () => {
           <Form.Control.Feedback type='invalid'>Username is required!</Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group className='mb-3'>
+        <Form.Group /* className='mb-3' */>
           <Form.Label htmlFor='email'>Email</Form.Label>
           <Form.Control
             type='email'
@@ -84,7 +107,7 @@ const SignupForm = () => {
           <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
         </Form.Group>
 
-        <Form.Group className='mb-3'>
+        <Form.Group /* className='mb-3' */>
           <Form.Label htmlFor='password'>Password</Form.Label>
           <Form.Control
             type='password'
