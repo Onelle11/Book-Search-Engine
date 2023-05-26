293442298b3eaed2322c06a453fef43c5dfcab99
Onelle11 <ofullente@gmail.com>
2023-05-25 23:28:21 -0400

    updated

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 /* mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks'); */
-/* mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', { */
-mongoose.connect("mongodb+srv://ofullente:zBIqH8PhlVbOYRBS@cluster0.cgoou9j.mongodb.net/googlebooks?retryWrites=true&w=majority", {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks', {
+/* mongoose.connect("mongodb+srv://ofullente:zBIqH8PhlVbOYRBS@cluster0.cgoou9j.mongodb.net/googlebooks?retryWrites=true&w=majority", { */
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
